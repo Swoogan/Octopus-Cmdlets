8f6fb710e6007b0092e61177b392329511bb9357
Colin Svingen <swoogan@gmail.com>
2014-08-05 22:01:50 -0400

    Broke the AddVariables cmdlet into begin, process, and end methods

--- Octopus.Cmdlets/AddVariable.cs
+++ Octopus.Cmdlets/AddVariable.cs
@@ -7,7 +7,9 @@ using Octopus.Platform.Model;
 
 namespace Octopus.Cmdlets
 {
-    [Cmdlet(VerbsCommon.Add, "OctoVariable")]
+    [Cmdlet(VerbsCommon.Add, 
+        "OctoVariable", 
+        DefaultParameterSetName = "Parts")]
     public class AddVariable : PSCmdlet
     {
         [Parameter(
@@ -64,18 +66,69 @@ namespace Octopus.Cmdlets
             )]
         public bool Sensitive { get; set; }
 
-        [Parameter(
-            Position = 0,
-            Mandatory = true,
-            ParameterSetName = "InputObject"
-            )]
-        public VariableResource InputObject { get; set; }
+        //[Parameter(
+        //    Position = 0,
+        //    Mandatory = true,
+        //    ValueFromPipeline = true,
+        //    ParameterSetName = "InputObject"
+        //    )]
+        //public VariableResource InputObject { get; set; }
 
         public AddVariable()
         {
             Sensitive = false;
         }
 
+        private OctopusRepository _octopus;
+        private VariableSetResource _variableSet;
+
+        protected override void BeginProcessing()
+        {
+            _octopus = (OctopusRepository)SessionState.PSVariable.GetValue("OctopusRepository");
+            if (_octopus == null)
+            {
+                throw new Exception(
+                    "Connection not established. Please connect to you Octopus Deploy instance with Connect-OctoServer");
+            }
+
+            WriteDebug("Got connection");
+
+            // Find the project that owns the variables we want to edit
+            var project = _octopus.Projects.FindByName(Project);
+
+            if (project == null)
+            {
+                const string msg = "Project '{0}' was found.";
+                throw new Exception(string.Format(msg, Project));
+            }
+
+            WriteDebug("Found project" + project.Id);
+
+            // Get the variables for editing
+            _variableSet = _octopus.VariableSets.Get(project.Link("Variables"));
+
+            WriteDebug("Found variable set" + _variableSet.Id);
+        }
+
+        protected override void ProcessRecord()
+        {
+            var variable = new VariableResource { Name = Name, Value = Value, IsSensitive = Sensitive };
+            var environments = _octopus.Environments.FindByNames(Environments);
+            var ids = environments.Select(environment => environment.Id).ToList();
+
+            variable.Scope.Add(ScopeField.Environment, new ScopeValue(ids));
+            _variableSet.Variables.Add(variable);
+        }
+
+        protected override void EndProcessing()
+        {
+            // Save the variables
+            _octopus.VariableSets.Modify(_variableSet);
+
+            WriteDebug("Modified the variable set");
+        }
+
+/*
         protected override void ProcessRecord()
         {
             var octopus = (OctopusRepository) SessionState.PSVariable.GetValue("OctopusRepository");
@@ -115,5 +168,6 @@ namespace Octopus.Cmdlets
 
             WriteDebug("Modified the variable set");
         }
+ **/
     }
 }
