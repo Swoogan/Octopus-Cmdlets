efdaa147e99be4a2f3fc14ec18b15f456c3044cb
Colin Svingen <swoogan@gmail.com>
2014-08-12 21:22:14 -0400

    Added psake build script to solution

---
+++ default.ps1
@@ -0,0 +1,25 @@
+﻿properties {
+  
+}
+
+task default -depends Release
+
+task Release { 
+  msbuild Octopus.Cmdlets/Octopus.Cmdlets.csproj /p:Configuration=Release
+}
+
+task Install -depends Release {
+	
+}
+
+task Debug { 
+  msbuild Octopus.Cmdlets/Octopus.Cmdlets.csproj /p:Configuration=Debug
+}
+
+task Clean { 
+  msbuild Octopus.Cmdlets/Octopus.Cmdlets.csproj /target:Clean
+}
+
+task ? -Description "Helper to display task info" {
+	Write-Documentation
+}
