3234ba92b41ac274ff973508198b58ec2f03a576
Colin Svingen <swoogan@gmail.com>
2014-08-15 23:02:59 -0400

    Added -Id to Get-Environment

--- Octopus.Cmdlets/GetEnvironment.cs
+++ Octopus.Cmdlets/GetEnvironment.cs
@@ -4,16 +4,24 @@ using Octopus.Client;
 
 namespace Octopus.Cmdlets
 {
-    [Cmdlet(VerbsCommon.Get, "Environment")]
+    [Cmdlet(VerbsCommon.Get, "Environment", DefaultParameterSetName = "ByName")]
     public class GetEnvironment : PSCmdlet
     {
         [Parameter(
+            ParameterSetName = "ByName",
             Position = 0,
             Mandatory = false,
             ValueFromPipeline = true,
             ValueFromPipelineByPropertyName = true,
-            HelpMessage = "The name of the VariableSet to look for.")]
+            HelpMessage = "The name of the envrionment to look for.")]
         public string[] Name { get; set; }
+
+        [Parameter(
+            ParameterSetName = "ById",
+            Position = 0,
+            Mandatory = true,
+            HelpMessage = "The id of the environment to look for.")]
+        public string Id { get; set; }
 
         private OctopusRepository _octopus;
 
@@ -27,14 +35,19 @@ namespace Octopus.Cmdlets
 
         protected override void ProcessRecord()
         {
-            if (Name == null)
+            if (Name != null)
             {
-                foreach (var environment in _octopus.Environments.GetAll())
+                foreach (var environment in _octopus.Environments.FindByNames(Name))
+                    WriteObject(environment);
+            }
+            else if (Id != null)
+            {
+                foreach (var environment in _octopus.Environments.FindMany(e => e.Id == Id))
                     WriteObject(environment);
             }
             else
             {
-                foreach (var environment in _octopus.Environments.FindByNames(Name))
+                foreach (var environment in _octopus.Environments.FindAll())
                     WriteObject(environment);
             }
         }
