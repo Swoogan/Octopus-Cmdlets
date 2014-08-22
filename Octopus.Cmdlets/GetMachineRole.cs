1768ac771568eba6113c13f830ec6c687674058f
Colin Svingen <swoogan@gmail.com>
2014-08-21 21:55:03 -0400

    Added get-machinerole cmdlet

---
+++ Octopus.Cmdlets/GetMachineRole.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using Octopus.Client;
+
+namespace Octopus.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "MachineRole", DefaultParameterSetName = "ByName")]
+    public class GetMachineRole : PSCmdlet
+    {
+        [Parameter(
+            ParameterSetName = "ByName",
+            Position = 0,
+            Mandatory = false,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The name of the machine to look for.")]
+        public string[] Name { get; set; }
+
+        private OctopusRepository _octopus;
+        private List<string> _roles;
+
+        protected override void BeginProcessing()
+        {
+            _octopus = (OctopusRepository) SessionState.PSVariable.GetValue("OctopusRepository");
+            if (_octopus == null)
+                throw new Exception(
+                    "Connection not established. Please connect to you Octopus Deploy instance with Connect-OctoServer");
+
+            _roles = _octopus.MachineRoles.GetAllRoleNames();
+        }
+
+        protected override void ProcessRecord()
+        {
+            if (Name == null)
+            {
+                foreach (var role in _roles)
+                    WriteObject(role);
+            }
+            else
+            {
+                var roles = from name in Name
+                    from role in _roles
+                    where role.Equals(name, StringComparison.InvariantCultureIgnoreCase)
+                    select role;
+                
+                foreach (var role in roles)
+                    WriteObject(role);
+            }
+        }
+    }
+}
--- Todo.md
+++ Todo.md
@@ -1,10 +1,15 @@
 Todo
 ----
-
-- Extend Add-Variable to add library vs variables
+- Implement Get-MachineRole 
+- Implement Get-Action
+- Add ScopeName script property to Get-Variable
 - Add machines, steps and roles to Add-Variable
+- Extend Add-Variable to add library vs variables
 - Warn if adding a duplicate variable
 - Add switch(es) to Add-Variable to -nowarn -skip and -error on duplicates
+- Write help file
+- Improve online documentation and move to wiki
+- Mention psake method for build and install in documentation
 
 Done
 ----
