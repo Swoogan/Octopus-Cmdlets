1aa959c6f682a7b4d7d99f5bacae5f80e4ca85a3
Colin Svingen <swoogan@gmail.com>
2015-02-26 17:28:10 -0500

    Added Get-Deployment cmdlet

---
+++ Octopus.Cmdlets/GetDeployment.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Text;
+using Octopus.Client;
+
+namespace Octopus.Cmdlets
+{
+    [Cmdlet(VerbsCommon.Get, "Deployment")]
+    public class GetDeployment : PSCmdlet
+    {
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            HelpMessage = "The name of the project to get deployments for.")]
+        public string Project { get; set; }
+
+        [Parameter(
+            Position = 1,
+            Mandatory = true,
+            HelpMessage = "The name of the release to get deployments for.")]
+        public string Release { get; set; }
+
+        private OctopusRepository _octopus;
+
+        protected override void BeginProcessing()
+        {
+            _octopus = Session.RetrieveSession(this);
+        }
+
+        protected override void ProcessRecord()
+        {
+            var project = _octopus.Projects.FindByName(Project);
+
+            if (project == null)
+                throw new Exception(string.Format("Project '{0}' was found.", Project));
+
+            var release = _octopus.Projects.GetReleases(project);
+            WriteObject(release);
+        }
+    }
+}
