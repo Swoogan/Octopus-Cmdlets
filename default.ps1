3003d52854f2996a1d4272350bb59a7ca2069770
Colin Svingen <swoogan@gmail.com>
2015-04-05 10:12:47 -0400

    Renamed module

--- default.ps1
+++ default.ps1
@@ -1,15 +1,24 @@
 ﻿properties {
-  $proj = "Octopus.Cmdlets/Octopus.Cmdlets.csproj"
-  $installPath = "Octopus.Cmdlets"
+  $proj = "Octopus-Cmdlets/Octopus-Cmdlets.csproj"
+  $installPath = "Octopus-Cmdlets"
 }
 
 function Copy-Files([string]$path) {
-	Copy-Item -Path ./Octopus.Cmdlets/Octopus.Cmdlets.Format.ps1xml -Destination $path
-	Copy-Item -Path ./Octopus.Cmdlets/Octopus.Cmdlets.psd1 -Destination $path
-	Copy-Item -Path ./Octopus.Cmdlets/Octopus.Cmdlets.Types.ps1xml -Destination $path
+	Copy-Item -Path ./Octopus-Cmdlets/Octopus-Cmdlets.Format.ps1xml -Destination $path
+	Copy-Item -Path ./Octopus-Cmdlets/Octopus-Cmdlets.psd1 -Destination $path
+	Copy-Item -Path ./Octopus-Cmdlets/Octopus-Cmdlets.Types.ps1xml -Destination $path
 
 	if (-not (Test-Path $path/en-US)) { mkdir $path/en-US }
-	Move-Item -Path $path/Octopus.Cmdlets.dll-help.xml -Destination $path/en-US/Octopus.Cmdlets.dll-help.xml -Force
+	Move-Item -Path $path/Octopus-Cmdlets.dll-help.xml -Destination $path/en-US/Octopus-Cmdlets.dll-help.xml -Force
+}
+
+function Install([string]$destination) {
+	if (-not (Test-Path $destination)) {
+		New-Item -Path $destination -ItemType directory | Out-Null
+		Write-Host "Created directory '$destination'" -ForegroundColor Green
+	}
+	Copy-Item -Path ./Octopus-Cmdlets/bin/Release/* -Destination $destination
+	Copy-Files $destination
 }
 
 task default -depends Release
@@ -18,31 +27,20 @@ task Release {
   msbuild $proj /p:Configuration=Release /p:SolutionDir=$PSScriptRoot
 }
 
-task Install -depends Install-ForMe, Release 
+task Install -depends Install-Personal, Release 
 
-task Install-ForMe -depends Release {
+task Install-Personal -depends Release {
 	$paths = $env:PSModulePath.Split(';')
 	$personal = $paths[0]	# suppose it's not guaranteed to be the first, oh well...
-	$installPath = Join-Path $personal -ChildPath $installPath
-	if (-not (Test-Path $installPath)) {
-		New-Item -Path $installPath -ItemType directory | Out-Null
-		Write-Host "Created directory '$installPath'" -ForegroundColor Green
-	}
-	Copy-Item -Path ./Octopus.Cmdlets/bin/Release/* -Destination $installPath
-	Copy-Files $installPath
+	$destination = Join-Path $personal -ChildPath $installPath
+	Install $destination
 }
 
-task Install-ForEveryone -depends Release {
+task Install-System -depends Release {
 	$paths = $env:PSModulePath.Split(';')
-	$personal = $paths[1]	# suppose it's not guaranteed to be the second, oh well...
-	$installPath = Join-Path $personal -ChildPath $installPath
-	if (-not (Test-Path $installPath)) {
-		New-Item -Path $installPath -ItemType directory | Out-Null
-		Write-Host "Created directory '$installPath'" -ForegroundColor Green
-	}
-	Copy-Item -Path ./Octopus.Cmdlets/bin/Release/* -Destination $installPath
-	
-	Copy-Files $installPath
+	$system = $paths[1]	# suppose it's not guaranteed to be the second, oh well...
+	$destination = Join-Path $system -ChildPath $installPath
+	Install $destination
 }
 
 task Clean { 
